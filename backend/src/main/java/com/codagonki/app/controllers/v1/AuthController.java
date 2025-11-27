18b45df639c5e19adf215231a9b795883ebeb101
zwaTOx <yakovva36@gmail.com>
2025-11-27 16:14:09 +0700

    add CorsConfig

--- backend/src/main/java/com/codagonki/app/controllers/v1/AuthController.java
+++ backend/src/main/java/com/codagonki/app/controllers/v1/AuthController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth/")
-@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
