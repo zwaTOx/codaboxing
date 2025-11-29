38cb131cca67a56365c55ce8050869cecee4077b
dofencode <kn.denisov@qwertex.ru>
2025-11-29 07:21:45 +0700

    Added ma balls

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,11 +3,12 @@ import headerComponent from './components/headerComponent.vue';
 </script>
 
 <template>
+  <div class="blue-ball"></div>
+  <div class="yellow-ball"></div>
   <headerComponent />
   <main class="main">
     <router-view ></router-view>
   </main>
-  
 </template>
 
 <script>
