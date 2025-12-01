f5089ce1c9dcbc7bf98e57976ca951a8bc294ff3
dofencode <kn.denisov@qwertex.ru>
2025-12-01 12:20:45 +0700

    Sending authorization header when matchmaking

--- frontend/src/api/duels.js
+++ frontend/src/api/duels.js
@@ -1,3 +1,4 @@
+import { loadFromCache } from "@/cache/cache";
 import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
@@ -5,6 +6,7 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const api = axios.create({
     baseURL: BASE_URL,
     headers: {
+        'Authorization': `Bearer ${loadFromCache('token')}`,
         'Content-Type': 'application/json'
     },
     withCredentials: true
