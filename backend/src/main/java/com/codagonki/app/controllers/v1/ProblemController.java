32967f5f3f4708cd4ac0ae5fbb3e745870144792
zwaTOx <yakovva36@gmail.com>
2025-12-04 00:02:46 +0700

    add submit code route

--- backend/src/main/java/com/codagonki/app/controllers/v1/ProblemController.java
+++ backend/src/main/java/com/codagonki/app/controllers/v1/ProblemController.java
@@ -6,15 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codagonki.app.DTO.Problem.ProblemResponse;
+import com.codagonki.app.DTO.Problem.SubmitRequest;
+import com.codagonki.app.DTO.TestCase.TestCaseListResponse;
 import com.codagonki.app.dependencies.CurrentUser;
 import com.codagonki.app.models.User;
 import com.codagonki.app.services.ProblemService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -28,5 +33,18 @@ public class ProblemController {
             @PathVariable Long duelId) {
         List<ProblemResponse> problems = problemService.getDuelProblems(duelId);
         return ResponseEntity.ok(problems);
+    }
+
+    @PostMapping("/duels/{duelId}/problems/{problemId}/submit")
+    public ResponseEntity<TestCaseListResponse> submitSolution(
+            @CurrentUser User user,
+            @PathVariable Long duelId,
+            @PathVariable Long problemId,
+            @Valid @RequestBody SubmitRequest submitRequest
+    ) {
+        TestCaseListResponse responseData = problemService.submitSolution(
+            user, duelId, problemId, submitRequest
+        );
+        return ResponseEntity.ok(responseData);
     }
 }
