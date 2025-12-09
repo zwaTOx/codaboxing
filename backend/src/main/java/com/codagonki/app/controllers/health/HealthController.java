82ac0c58558ce8fb64a0ec0d950b8d0179a2c696
zwaTOx <yakovva36@gmail.com>
2025-12-09 19:37:05 +0700

    add HealthController

---
+++ backend/src/main/java/com/codagonki/app/controllers/health/HealthController.java
@@ -0,0 +1,18 @@
+package com.codagonki.app.controllers.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/")
+public class HealthController {
+
+    @GetMapping("/ping")
+    public String app_health_check() {
+        return "pong";
+    }
+}
