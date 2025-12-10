2b11dacdb3173fb301a105f2cc32d99cb932908d
zwaTOx <yakovva36@gmail.com>
2025-12-10 22:57:20 +0700

    add DuelPlayerStatus model

---
+++ backend/src/main/java/com/codagonki/app/models/DuelPlayerStatus.java
@@ -0,0 +1,60 @@
+package com.codagonki.app.models;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "duel_player_status")
+@Data 
+@Builder 
+@NoArgsConstructor
+@AllArgsConstructor
+public class DuelPlayerStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "duel_id")
+    private Duel duel;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(name = "duel_problem_progress", nullable = false, columnDefinition = "jsonb")
+    private Map<String, Object> duelProblemProgress;
+    
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public enum SolveStatus {
+        NOT_ATTEMPTED,
+        ATTEMPTED, 
+        SOLVED,     
+    }
+}
